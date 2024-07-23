8ebcf259f3ea423ad2abd99d297b669c780a9c72
Daniel Lemeszenski <daniel.lemeszenski@grupomarista.org.br>
2024-07-22 22:53:53 -0300

    'initial'

--- src/Main.java
+++ src/Main.java
@@ -24,40 +24,19 @@ class Solution {
 	*/
 
     public static void levelOrder(Node root) {
-        HashMap<Integer,HashMap<Integer,Node>> hm = new HashMap();
-        hm.put(0,new HashMap());
-        //esquerda
-        HashMap<Integer, Node> hmcurr = new HashMap();
-        int altura = 1;
-        Node curr = root;
-        hmcurr.put(0, curr);
-        hm.put(altura, hmcurr);
-        while (hm.get(altura).size() > 0) {
-            hmcurr = new HashMap();
-            for (int i = 0; i < hm.get(altura).size(); i++) {
-                curr = hm.get(altura).get(i);
-                if(curr == null){
-                    continue;
-                }
-
-                if (curr.left != null) {
-                    hmcurr.put(i, curr.left);
-                }
-                if (curr.right != null) {
-                    hmcurr.put(i * 2 + 1, curr.right);
-                }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print(current.data + " ");
+            if (current.left != null) {
+                queue.add(current.left);
             }
-            altura++;
-            hm.put(altura, hmcurr);
-        }
-        for (int i = 0; i < hm.size(); i++) {
-            for (int j = 0; j < hm.get(i).size(); j++) {
-                Node node = hm.get(i).get(j);
-                if (node != null) {
-                    System.out.print(node.data + " ");
-                }
+            if (current.right != null) {
+                queue.add(current.right);
             }
         }
+        
 
     }
 
